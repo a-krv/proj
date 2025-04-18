e85d759d3991d1f35dfb119a10c5d62c45fe1803
Aleksandr Karavaikin <ache-7@yandex.ru>
2025-04-18 21:50:30 +0300

    changed added Box class

---
+++ src/main/java/com/stepup/proj/Box.java
@@ -0,0 +1,13 @@
+package com.stepup.proj;
+
+public class Box {
+    private int size;
+
+    public Box(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
--- src/main/java/com/stepup/proj/MainApp.java
+++ src/main/java/com/stepup/proj/MainApp.java
@@ -4,5 +4,6 @@ public class MainApp {
     public static void main(String[] args) {
         System.out.println("dddd");
         System.out.println("1111111111");
+        System.out.println("22221");
     }
 }
