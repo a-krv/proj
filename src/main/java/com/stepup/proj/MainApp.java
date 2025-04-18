3727cb96c87f9600bd663acbdfbb39b9010356f1
Aleksandr Karavaikin <ache-7@yandex.ru>
2025-04-18 22:16:24 +0300

    string removed

--- src/main/java/com/stepup/proj/MainApp.java
+++ src/main/java/com/stepup/proj/MainApp.java
@@ -2,13 +2,6 @@ package com.stepup.proj;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("dddd");
         System.out.println("1111111111");
-        System.out.println("----");
-        System.out.println("----");
-        System.out.println("----");
-        System.out.println("----");
-        System.out.println("----");
-        System.out.println("----");
     }
 }
