8de9c22e02ae3364fd990313b0576b5dd3286dd8
Aleksandr Karavaikin <ache-7@yandex.ru>
2025-04-18 21:40:33 +0300

    added

--- src/main/java/com/stepup/proj/MainApp.java
+++ src/main/java/com/stepup/proj/MainApp.java
@@ -3,5 +3,6 @@ package com.stepup.proj;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("dddd");
+        System.out.println("1111111111");
     }
 }
