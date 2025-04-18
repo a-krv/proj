27dc1f3159fb4e17dd988828e25c143dfd83e59c
Aleksandr Karavaikin <ache-7@yandex.ru>
2025-04-18 22:17:26 +0300

    +1 string added

--- src/main/java/com/stepup/proj/MainApp.java
+++ src/main/java/com/stepup/proj/MainApp.java
@@ -3,5 +3,6 @@ package com.stepup.proj;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("1111111111");
+        System.out.println("565");
     }
 }
